b97a3f95ef9f6bf2cc9087a0a4365f4edcc64b04
ajayraj201651006 <ajayraj003016@gmail.com>
2019-02-13 19:54:18 +0530

    create menu component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
+import Menu from './components/MenuComponent';
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
          </div>
        </Navbar>
+       <Menu />
       </div>
     );
   }
