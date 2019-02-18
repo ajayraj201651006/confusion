48ed8df40e1772f8dcc9b15cd4640ce2cb0f0b67
ajayraj201651006 <ajayraj003016@gmail.com>
2019-02-18 18:34:48 +0530

    convert menu into functional

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,20 +1,23 @@
-import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardTitle, CardBody, CardText } from 'reactstrap';
+import React from 'react';
+import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
-class Menu extends Component {
-    
-    render() {
+    function RenderMenuItem({ dish, clicked }) {
+        return (
+            <Card 
+               onClick={() => clicked(dish.id)}>
+                <CardImg width="100%" src={dish.image} alt={dish.name} />
+                <CardImgOverlay>
+                    <CardTitle tag="h5">{dish.name}</CardTitle>
+                </CardImgOverlay>
+            </Card>
+        );
+    }
 
-        const menu = this.props.dishes.map((dish) => {
+    const Menu = (props) => {
+        const menu = props.dishes.map((dish) => {
             return (
                 <div key={dish.id} className="col-12 col-md-5 m-1">
-                   <Card 
-                     onClick={() => this.props.clicked(dish.id)}>
-                       <CardImg width="100%" src={dish.image} alt={dish.name} />
-                       <CardImgOverlay>
-                           <CardTitle tag="h5">{dish.name}</CardTitle>
-                       </CardImgOverlay>
-                   </Card>
+                   <RenderMenuItem dish={dish} clicked={props.clicked} />
                 </div>
             );
         });
@@ -27,6 +30,7 @@ class Menu extends Component {
              </div>   
         );
     }
-}
+
+        
 
 export default Menu;
