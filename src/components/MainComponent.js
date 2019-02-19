40eab0a2d06d0f35b3536504209c93fc36769fe2
ajayraj201651006 <ajayraj003016@gmail.com>
2019-02-19 20:01:31 +0530

    Enabling Route

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,27 +4,37 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent.js';
 import { DISHES } from '../shared/Dishes';
 import DishDetail from './DishDetailComponent';
+import Home from './HomeComponent';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 
 
 class Main extends Component {
 
   state = {
-        dishes: DISHES,
-        selectedDish: null
+        dishes: DISHES
+        // selectedDish: null
     }
  
-  onDishSelect = (dish) => {
-        this.setState({selectedDish: dish});
-    }
+  // onDishSelect = (dish) => {
+  //       this.setState({selectedDish: dish});
+  //   }
 
   render() {
+    const HomePage = () => {
+       return (
+          <Home />
+       );
+    }
+
     return (
       <div>
        <Header />
-       <Menu dishes={this.state.dishes} 
-             clicked={(dishId)=>{this.onDishSelect(dishId)}} />
-       <DishDetail selectDish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+       <Switch>
+         <Route path="/home" component={HomePage} />
+         <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+         <Redirect to="/home" />
+       </Switch>
        <Footer />
       </div>
     );
