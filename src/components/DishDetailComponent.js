fef4d8550acfc35f7552e3c80e416a17d9e8a903
ajayraj201651006 <ajayraj003016@gmail.com>
2019-02-20 13:42:30 +0530

    update dishDetail

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Card, CardImg, CardTitle, CardBody, CardText } from 'reactstrap';
-
+import { Card, CardImg, CardTitle, CardBody, CardText, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
     function RenderComments ({ comment }) {
         const rdc = comment.map((cmt)=>{
@@ -49,11 +49,21 @@ import { Card, CardImg, CardTitle, CardBody, CardText } from 'reactstrap';
         return (
             <div className="container">
             <div className="row">
+                <Breadcrumb>
+                    <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>{props.selectDish.name}</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>{props.selectDish.name}</h3>
+                    <hr />
+                </div>
+            </div>
+            <div className="row">
                <div className="col-12 col-md-5 m-1">
                    <RenderDish dish={props.selectDish} />
                </div>
                <div className="col-12 col-md-5 m-1">
-                   <RenderComments comment={props.selectDish.comments} />
+                   <RenderComments comment={props.comments} />
                </div>
             </div>
             </div>
--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -19,7 +19,7 @@ class Header extends Component {
                     <div className="container">
                        <NavbarToggler onClick={this.toggleNav} />
                        <NavbarBrand className="mr-auto" href="/">
-                          <img src="assets/images/logo.png" width="30" height="41" 
+                          <img src="/assets/images/logo.png" width="30" height="41" 
                              alt="Ristorante Con Fusion" />
                        </NavbarBrand>
                        <Collapse isOpen={this.state.isNavOpen} navbar>
