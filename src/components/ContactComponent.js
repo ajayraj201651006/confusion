c5c7d14dfccd5f9b1a564046f87029bf4748c003
ajayraj201651006 <ajayraj003016@gmail.com>
2019-02-22 20:29:04 +0530

    update controll form

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -7,6 +7,7 @@ class Contact extends Component {
     state = {
         firstname: '',
         lastname: '',
+        email: '',
         telnum: '',
         agree: false,
         contactType: 'Tel.',
@@ -64,6 +65,64 @@ class Contact extends Component {
                                     <Input type="text" id="firstname" name="firstname"
                                             placeholder="First Name"
                                             value={this.state.firstname} />
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label htmlFor="lastname" md={2}>Last Name</Label>
+                                <Col md={10}>
+                                    <Input type="text" id="lastname" name="lastname"
+                                            placeholder="Last Name"
+                                            value={this.state.lastname} />
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label htmlFor="email" md={2}>Email</Label>
+                                <Col md={10}>
+                                    <Input type="email" id="email" name="email"
+                                            placeholder="Email"
+                                            value={this.state.email} />
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label htmlFor="telnum" md={2}>Contact Tel.</Label>
+                                <Col md={10}>
+                                    <Input type="tel" id="telnum" name="telnum"
+                                            placeholder="Tel."
+                                            value={this.state.telnum} />
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Col md={{size: 6, offset: 2}}>
+                                    <FormGroup check>
+                                        <Label check>
+                                            <Input type="checkbox" name="agree"
+                                                   checked={this.state.agree} /> {' '}
+                                                   <strong>May We Contact You</strong>
+
+                                        </Label>
+                                    </FormGroup>
+                                </Col>
+                                <Col md={{size: 3, offset: 1}}>
+                                    <Input type="select" name="contactType"
+                                      value={this.state.contactType}>
+                                      <option>Tel.</option>
+                                      <option>Email</option>
+                                    </Input>
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label htmlFor="message" md={2}>Your Feedback</Label>
+                                <Col md={10}>
+                                    <Input type="textarea" id="message" name="message"
+                                            rows="12"
+                                            value={this.state.message} />
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Col md={{size: 10, offset: 2}}>
+                                    <Button type="submit" color="primary">
+                                       Send Feedback
+                                    </Button>
                                 </Col>
                             </FormGroup>
                         </Form>
